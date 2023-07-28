ce48fb0bdf39253d708d4b0901447b3db558003d
meshnjihia <meshworks7@gmail.com>
2023-07-28 06:32:59 +0300

    types correction

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -1,8 +1,8 @@
-import { Billboard } from "@/types";
+import { BillboardType } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
 
-export const getBillboard = async (id: string): Promise<Billboard> => {
+export const getBillboard = async (id: string): Promise<BillboardType> => {
     const res = await fetch(`${URL}/${id}`)
 
     return res.json()
